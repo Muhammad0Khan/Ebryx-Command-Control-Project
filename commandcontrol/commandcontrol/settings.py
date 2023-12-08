26983544ff255d0845c317ab9c01fcf554545306
Muhammad0Khan <f2020-447@bnu.edu.pk>
2023-12-08 12:44:04 +0500

    attempting deployment fixes

--- commandcontrol/commandcontrol/settings.py
+++ commandcontrol/commandcontrol/settings.py
@@ -162,3 +162,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapp/static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
