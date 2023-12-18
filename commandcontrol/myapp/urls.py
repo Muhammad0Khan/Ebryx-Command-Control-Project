8244a62dae4691e58c92fefb064e06498ecf37b4
Muhammad0Khan <f2020-447@bnu.edu.pk>
2023-12-18 17:02:51 +0500

    removed unused authentication app, added apis

--- commandcontrol/myapp/urls.py
+++ commandcontrol/myapp/urls.py
@@ -1,21 +1,22 @@
 from django.urls import path, include
 from .views import cpu_info_view, profile_view, logout_view
-from authentication.views import SignUpView, CustomLoginView
+# from authentication.views import SignUpView, CustomLoginView
 from . import views
 from django.contrib import admin
-from .views import installed_apps
+from .views import *
 
 
 urlpatterns = [
     path("cpu_info/", cpu_info_view, name="cpu_info"),
     path("remote_cpu_info_api/", views.remote_cpu_info_api, name="remote_cpu_info_api"),
-    path("signup/", SignUpView.as_view(), name="signup"),
-    path("login/", CustomLoginView.as_view(), name="login"),
+    # path("signup/", SignUpView.as_view(), name="signup"),
+    # path("login/", CustomLoginView.as_view(), name="login"),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/profile/", profile_view, name="profile"),
     path("logout/", logout_view, name="logout"),
     path('admin/', admin.site.urls),
-    path('installed-apps/', installed_apps, name='installed_apps'),
+    # path('installed-apps/', installed_apps, name='installed_apps'),
+    path('api/installed_apps/', InstalledAppAPIView.as_view(), name='installed_apps'),
 
 
 ]
