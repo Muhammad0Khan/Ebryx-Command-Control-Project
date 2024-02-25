ce54351293899d30df9ccf9ff180b2f096e48135
MohdAleeRehman <mohdaleerehman@gmail.com>
2024-02-25 05:34:42 +0500

    missing files bug fixed. username not shown big fixed.

--- commandcontrol/myapp/views.py
+++ commandcontrol/myapp/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import logout, authenticate, login
 from datetime import datetime
-import subprocess,json,time, psutil
+import subprocess, json, time, psutil
 from firebase_admin import db
 
 from rest_framework.decorators import api_view
@@ -53,53 +53,53 @@ def logout_view(request):
     return redirect("login")
 
 
-# added apis here 
-@api_view(['GET'])
+# added apis here
+@api_view(["GET"])
 def generate_token(request):
     # Generate a unique token
     token = get_random_string(length=40)
 
     # Store the token in Firestore
-    ref = db.reference('tokens')
-    ref.push().set({'token': token})
+    ref = db.reference("tokens")
+    ref.push().set({"token": token})
 
-    ref_create_online_status= db.reference('status')
-    ref_create_online_status.push().set({'token': token, 'status':"offline"})
+    ref_create_online_status = db.reference("status")
+    ref_create_online_status.push().set({"token": token, "status": "offline"})
 
     # Return the token in the API response
-    return Response({'token': token})
-
+    return Response({"token": token})
 
 
 def check_token(request, token):
     try:
         # Reference to the Firebase Realtime Database node where you store tokens
-        tokens_ref = db.reference('/tokens')
+        tokens_ref = db.reference("/tokens")
 
         # Query the database to check if the token exists
-        query = tokens_ref.order_by_child('token').equal_to(token).get()
+        query = tokens_ref.order_by_child("token").equal_to(token).get()
         update_token_status(token)
         set_status_offline(token, delay=30)
 
         if query:
-            return JsonResponse({'exists': True})
+            return JsonResponse({"exists": True})
         else:
-            return JsonResponse({'exists': False})
+            return JsonResponse({"exists": False})
     except Exception as e:
-        return JsonResponse({'error': str(e)})
+        return JsonResponse({"error": str(e)})
 
 
 @csrf_exempt
 def save_firebase_token(token_value):
     APIToken.objects.create(token_value=token_value)
 
+
 class InstalledAppAPIView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = InstalledAppSerializer(data=request.data)
         if serializer.is_valid():
             # Save to the Firebase Realtime Database
-            token = serializer.validated_data.get('token')
-            firebase_ref = db.reference(f'/installed_apps/{token}')
+            token = serializer.validated_data.get("token")
+            firebase_ref = db.reference(f"/installed_apps/{token}")
             firebase_ref.set(serializer.data)
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -134,6 +134,7 @@ def signup_view(request):
 
     return render(request, "registration/signup.html", {"form": form})
 
+
 def firebase_auth_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
@@ -142,6 +143,7 @@ def firebase_auth_required(view_func):
         return view_func(request, *args, **kwargs)
 
     return _wrapped_view
+
 
 def login_view(request):
     if request.method == "POST":
@@ -178,37 +180,46 @@ def login_view(request):
 
     return render(request, "registration/login.html", {"form": form})
 
+
 @firebase_auth_required
 def dashboard_view(request):
     # Assuming you have a Firebase Admin instance initialized
     # Reference to the 'tokens' collection
-    tokens_ref = db.reference('tokens')
+    tokens_ref = db.reference("tokens")
     # Reference to the 'status' collection
     status_ref = db.reference("status")
     # Retrieve token data
     tokens_data = tokens_ref.order_by_child("token").get()
     # Retrieve status data
-    status_data = status_ref.order_by_child('token').get()
+    status_data = status_ref.order_by_child("token").get()
     # print(status_data)
     # Create a list of tokens with details, including matching status
     tokens = []
     for token_key, token in tokens_data.items():
         # Check if there is a corresponding status for the token
-        status_key = next((key for key, value in status_data.items() if value['token'] == token['token']), None)
+        status_key = next(
+            (
+                key
+                for key, value in status_data.items()
+                if value["token"] == token["token"]
+            ),
+            None,
+        )
         status = status_data.get(status_key, {}) if status_key else {}
 
         # Create a dictionary with token details and status
         token_details = {
-            'token': token['token'],
-            'details_url': f'/token-details/{token["token"]}/',
-            'cpu_info': f'/api/cpu_info/{token["token"]}/',
-            'status': status.get('status', 'N/A'),  # Use 'N/A' if no status found
+            "token": token["token"],
+            "details_url": f'/token-details/{token["token"]}/',
+            "cpu_info": f'/api/cpu_info/{token["token"]}/',
+            "status": status.get("status", "N/A"),  # Use 'N/A' if no status found
         }
         print(token_details)
 
         tokens.append(token_details)
 
-    return render(request, 'dashboard.html', {'tokens': tokens})
+    return render(request, "dashboard.html", {"tokens": tokens})
+
 
 @firebase_auth_required
 def cpu_dashboard_view(request):
@@ -243,205 +254,229 @@ def cpu_dashboard_view(request):
 
 def token_details_view(request, token):
     # Fetch data from the installed_apps collection for the specific token
-    apps_ref = db.reference('installed_apps').child(token)
+    apps_ref = db.reference("installed_apps").child(token)
+    cpu_data_ref = db.reference("cpu_data").child(token)
+    username = cpu_data_ref.get().get("data", [])[-1].get("username", "")
     token_data = apps_ref.get()
 
     if token_data:
-        installed_apps_data = token_data.get('data', [])
+        installed_apps_data = token_data.get("data", [])
     else:
         installed_apps_data = []
 
-    return render(request, 'token_details.html', {'token': token, 'installed_apps': installed_apps_data})
+    return render(
+        request,
+        "token_details.html",
+        {"username": username, "installed_apps": installed_apps_data},
+    )
 
 
 def update_token_status(token):
-    print('function called ')
+    print("function called ")
     # Reference to the 'status' collection
-    status_ref = db.reference('status')
+    status_ref = db.reference("status")
 
     # Find the status entry for the given token
-    status_data = status_ref.order_by_child('token').equal_to(token).get()
+    status_data = status_ref.order_by_child("token").equal_to(token).get()
 
-    print (status_data)
+    print(status_data)
 
     # Update the status to 'online' if a matching status is found
     if status_data:
         status_key = list(status_data.keys())[0]
-        status_ref.child(status_key).update({'status': 'online'})
+        status_ref.child(status_key).update({"status": "online"})
         print(f"Status updated to 'online' for token: {token}")
     else:
         print(f"No matching status found for token: {token}")
 
 
-
 def set_status_offline(token, delay=30):
     # Reference to the 'status' collection
-    status_ref = db.reference('status')
+    status_ref = db.reference("status")
 
     # Find the status entry for the given token
-    status_data = status_ref.order_by_child('token').equal_to(token).get()
+    status_data = status_ref.order_by_child("token").equal_to(token).get()
 
     # Update the status to 'offline' after a delay if a matching status is found
     if status_data:
         status_key = list(status_data.keys())[0]
-        
+
         # Sleep for the specified delay in seconds
         time.sleep(delay)
 
         # Update the status to 'offline'
-        status_ref.child(status_key).update({'status': 'offline'})
+        status_ref.child(status_key).update({"status": "offline"})
         print(f"Status updated to 'offline' for token: {token}")
     else:
         print(f"No matching status found for token: {token}")
 
 
-@api_view(['DELETE'])
+@api_view(["DELETE"])
 def delete_token(request, token):
     try:
         # Reference to the 'status' collection
-        status_ref = db.reference('status')
+        status_ref = db.reference("status")
 
         # Find the status entry for the given token
-        status_data = status_ref.order_by_child('token').equal_to(token).get()
+        status_data = status_ref.order_by_child("token").equal_to(token).get()
 
         # Delete the token entry if a matching status is found
         if status_data:
             status_key = list(status_data.keys())[0]
             status_ref.child(status_key).delete()
-            return Response({'success': True, 'message': f'Token {token} deleted successfully'})
+            return Response(
+                {"success": True, "message": f"Token {token} deleted successfully"}
+            )
         else:
-            return Response({'success': False, 'message': f'No matching status found for token: {token}'})
+            return Response(
+                {
+                    "success": False,
+                    "message": f"No matching status found for token: {token}",
+                }
+            )
     except Exception as e:
-        return Response({'success': False, 'error': str(e)})
-    
+        return Response({"success": False, "error": str(e)})
+
 
 @csrf_exempt
 @require_POST
 def store_cpu_data(request):
     try:
         data = json.loads(request.body)
-        token = data.get('token')
+        token = data.get("token")
 
         if not token:
             response_data = {
-                'status': 'error',
-                'message': 'Token is required in the JSON data.',
+                "status": "error",
+                "message": "Token is required in the JSON data.",
             }
             return JsonResponse(response_data, status=400)
 
         # Assuming 'cpu_data' is the reference to the desired location in your RTDB
-        cpu_data_ref = db.reference(f'cpu_data/{token}')
+        cpu_data_ref = db.reference(f"cpu_data/{token}")
 
         # Fetch the existing data array or initialize an empty array
-        existing_data = cpu_data_ref.child('data').get() or []
+        existing_data = cpu_data_ref.child("data").get() or []
 
         # Append the new data section to the array
         existing_data.append(data)
 
         # Update the RTDB with the new data array
-        cpu_data_ref.update({'data': existing_data})
+        cpu_data_ref.update({"data": existing_data})
 
         response_data = {
-            'status': 'success',
-            'message': 'CPU data stored successfully',
-            'data_id': len(existing_data) - 1,  # Index of the last appended data section
+            "status": "success",
+            "message": "CPU data stored successfully",
+            "data_id": len(existing_data)
+            - 1,  # Index of the last appended data section
         }
 
         return JsonResponse(response_data)
 
     except json.JSONDecodeError:
         response_data = {
-            'status': 'error',
-            'message': 'Invalid JSON data',
+            "status": "error",
+            "message": "Invalid JSON data",
         }
         return JsonResponse(response_data, status=400)
 
     except Exception as e:
         response_data = {
-            'status': 'error',
-            'message': f'An error occurred: {str(e)}',
+            "status": "error",
+            "message": f"An error occurred: {str(e)}",
         }
         return JsonResponse(response_data, status=500)
 
 
 def cpu_info_page(request, token):
     try:
-        cpu_data_ref = db.reference(f'cpu_data/{token}')
+        cpu_data_ref = db.reference(f"cpu_data/{token}")
 
         # Fetch the CPU data for the specified token
         cpu_data = cpu_data_ref.get()
 
         if cpu_data:
             # Get the last data section (assuming it's a list of dictionaries)
-            last_data_section = cpu_data.get('data', [])[-1]
+            last_data_section = cpu_data.get("data", [])[-1]
 
             context = {
-                'system_name': last_data_section.get('system_name', ''),
-                'hostname': last_data_section.get('hostname', ''),
-                'threads': last_data_section.get('threads', ''),
-                'cpu_count': last_data_section.get('cpu_count', ''),
-                'cpu_usage': last_data_section.get('data', {}).get('cpu_usage', ''),
-                'cpu_frequency': last_data_section.get('data', {}).get('cpu_frequency', ''),
-                'per_cpu_percent': last_data_section.get('data', {}).get('per_cpu_percent', ''),
-                'timestamp': last_data_section.get('data', {}).get('timestamp', ''),
-                
+                "system_name": last_data_section.get("system_name", ""),
+                "hostname": last_data_section.get("hostname", ""),
+                "threads": last_data_section.get("threads", ""),
+                "cpu_count": last_data_section.get("cpu_count", ""),
+                "cpu_usage": last_data_section.get("data", {}).get("cpu_usage", ""),
+                "cpu_frequency": last_data_section.get("data", {}).get(
+                    "cpu_frequency", ""
+                ),
+                "per_cpu_percent": last_data_section.get("data", {}).get(
+                    "per_cpu_percent", ""
+                ),
+                "timestamp": last_data_section.get("data", {}).get("timestamp", ""),
             }
 
-            print (context)
-            return render(request, 'cpu_info.html', context)
+            print(context)
+            return render(request, "cpu_info.html", context)
         else:
-            return render(request, 'cpu_info.html', {'error_message': f'No data found for token: {token}'})
+            return render(
+                request,
+                "cpu_info.html",
+                {"error_message": f"No data found for token: {token}"},
+            )
     except Exception as e:
-        return render(request, 'cpu_info.html', {'error_message': f'An error occurred: {str(e)}'})
-    
+        return render(
+            request, "cpu_info.html", {"error_message": f"An error occurred: {str(e)}"}
+        )
+
 
 # /api/network_data
+
 
 @csrf_exempt
 @require_POST
 def store_network_data(request):
     try:
         data = json.loads(request.body)
-        token = data.get('token')
+        token = data.get("token")
 
         if not token:
             response_data = {
-                'status': 'error',
-                'message': 'Token is required in the JSON data.',
+                "status": "error",
+                "message": "Token is required in the JSON data.",
             }
             return JsonResponse(response_data, status=400)
 
         # Assuming 'cpu_data' is the reference to the desired location in your RTDB
-        cpu_data_ref = db.reference(f'network_data/{token}')
+        cpu_data_ref = db.reference(f"network_data/{token}")
 
         # Fetch the existing data array or initialize an empty array
-        existing_data = cpu_data_ref.child('data').get() or []
+        existing_data = cpu_data_ref.child("data").get() or []
 
         # Append the new data section to the array
         existing_data.append(data)
 
         # Update the RTDB with the new data array
-        cpu_data_ref.update({'data': existing_data})
+        cpu_data_ref.update({"data": existing_data})
 
         response_data = {
-            'status': 'success',
-            'message': 'network data stored successfully',
-            'data_id': len(existing_data) - 1,  # Index of the last appended data section
+            "status": "success",
+            "message": "network data stored successfully",
+            "data_id": len(existing_data)
+            - 1,  # Index of the last appended data section
         }
 
         return JsonResponse(response_data)
 
     except json.JSONDecodeError:
         response_data = {
-            'status': 'error',
-            'message': 'Invalid JSON data',
+            "status": "error",
+            "message": "Invalid JSON data",
         }
         return JsonResponse(response_data, status=400)
 
     except Exception as e:
         response_data = {
-            'status': 'error',
-            'message': f'An error occurred: {str(e)}',
+            "status": "error",
+            "message": f"An error occurred: {str(e)}",
         }
         return JsonResponse(response_data, status=500)
 
